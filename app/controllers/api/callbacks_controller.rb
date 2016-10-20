48d11441dd890b90ea4859c03cfdfad63a9355b6
alisafrunza <alisafrunza@gmail.com>
2016-10-20 18:51:23 +0300

    handles success callback

--- app/controllers/api/callbacks_controller.rb
+++ app/controllers/api/callbacks_controller.rb
@@ -3,6 +3,18 @@ class CallbacksController < ApplicationController
 
 	def success
 		pp params
+
+		login_hash = Saltedge::Client.new.show_login(params[:login_id])
+		login_hash["salt_id"] = login_hash.delete("id")
+		login = Login.find_or_create_by(salt_id: login_hash["salt_id"])
+		login.update_attributes(login_hash)
+
+		accounts = Saltedge::Client.new.get_accounts(current_login)
+		save_fresh_accounts(accounts)
+		transactions = Saltedge::Client.new.get_transactions(current_login)
+		save_fresh_transactions(transactions)
+
+		render :nothing => true
 		#pseudo
 		#login exists fetch login compair attributes
 		#login doesn't exist, create login
