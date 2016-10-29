effabad26d54416ddbbb16c35ba2e78f7e15fd69
alisafrunza <alisafrunza@gmail.com>
2016-10-29 18:34:03 +0300

    updates refresh button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-	def can_be_refreshed?(login)
-		return true if login.next_refresh_possible_at.blank?
-		Time.now > login.next_refresh_possible_at
-	end
+  def can_be_refreshed?(login)
+    return true if login.next_refresh_possible_at.blank?
+    Time.now > login.next_refresh_possible_at
+  end
 end
